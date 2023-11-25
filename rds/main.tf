86408183f1dccb104b7c8ad7fa726680dc36681d
tatianazubco11 <142647496+tatianazubco11@users.noreply.github.com>
2023-11-25 15:29:31 -0600

    update

--- rds/main.tf
+++ rds/main.tf
@@ -11,10 +11,8 @@ data "terraform_remote_state" "vpc" {
 }
 
 
-# Creates subnet group with private subnet
+# Creates subnet group with private subnets
 resource "aws_db_subnet_group" "default" {
   name       = "main"
-  subnet_ids = [
-    data.terraform_remote_state.vpc.outputs.private_subnets
-  ]
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
 }
